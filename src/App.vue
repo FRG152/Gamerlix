d82763ee559341267d7f47b8bd481f1494b2394f
FRG152 <romanflavio3@hotmail.com>
2025-06-09 10:53:00 -0300

    Remove custom cursor implementation from App.vue and update base.css to restore default cursor behavior.

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,15 @@
 <script setup>
 import Hero from "./components/Hero.vue";
+import { ref } from "vue";
 import Navbar from "./components/Navbar.vue";
 import Firebrands from "./components/Firebrands.vue";
-import { ref, onMounted } from "vue";
 import { leftFirebrands, rightFirebrands } from "./constants";
 
 const container = ref(null);
-const cursor = ref(null);
-
-onMounted(() => {
-  window.addEventListener("mousemove", (e) => {
-    cursor.value.style.left = `${e.clientX}px`;
-    cursor.value.style.top = `${e.clientY}px`;
-  });
-});
 </script>
 
 <template>
   <main ref="container" class="container">
-    <div ref="cursor" class="custom-cursor"></div>
     <Navbar />
     <Hero />
     <Firebrands
@@ -55,17 +46,5 @@ onMounted(() => {
     var(--bgColor-rbga-secondary) 100%
   );
   overflow: hidden;
-}
-.custom-cursor {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100px;
-  height: 100px;
-  transform: translate(-50%, -50%);
-  transition: all 0.1s;
-  border-radius: 50%;
-  pointer-events: none;
-  background-color: #000000ce;
 }
 </style>
